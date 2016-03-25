1fb1c032dae0feecff02295c0cf7a850ad195d91
Jan-Erik Rediger <janerik@fnordig.de>
2016-03-25 15:07:00 +0100

    chore: Install OpenSSL

--- mk/appveyor.bat
+++ mk/appveyor.bat
@@ -8,6 +8,7 @@ SET PATH=C:\Program Files (x86)\MSBuild\%TOOLCHAIN_VERSION%\Bin;%PATH%
 set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio %TOOLCHAIN_VERSION%\VC\vcvarsall.bat"
 
 if [%Platform%] NEQ [x64] goto win32
+set BITS=64
 set TARGET_ARCH=x86_64
 set TARGET_PROGRAM_FILES=%ProgramFiles%
 call %VCVARSALL% amd64
@@ -17,6 +18,7 @@ goto download
 :win32
 echo on
 if [%Platform%] NEQ [Win32] exit 1
+set BITS=32
 set TARGET_ARCH=i686
 set TARGET_PROGRAM_FILES=%ProgramFiles(x86)%
 call %VCVARSALL% amd64_x86
@@ -39,6 +41,13 @@ start /wait msiexec /i build\rust-%RUST%-%TARGET_ARCH%-pc-windows-msvc.msi INSTA
 if %ERRORLEVEL% NEQ 0 exit 1
 
 set PATH="%TARGET_PROGRAM_FILES%\Rust %RUST%\bin";%PATH%
+
+set OPENSSL_URL="http://slproweb.com/download/Win${env:BITS}OpenSSL-1_0_2g.exe"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%OPENSSL_URL%', 'build\Win%BITS%OpenSSL-1_0_2g.exe')"
+build\Win%BITS%OpenSSL-1_0_2g.exe /SILENT /VERYSILENT /SP- /DIR="C:\OpenSSL"
+SET PATH=%PATH%;C:\Program Files (x86)\Rust\bin
+SET PATH=%PATH%;C:\MinGW\bin
+
 
 if [%Configuration%] == [Release] set CARGO_MODE=--release
 
